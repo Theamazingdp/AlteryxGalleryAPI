22b8926aa6840ebc31bd6790b3d24265a876cab8
Paul Houghton <paul.j.houghton@gmail.com>
2023-09-12 01:50:31 +0100

    renamed test file to conform to pytest reqs

--- src/tests/test_gallery_connection.py
+++ src/tests/test_gallery_connection.py
@@ -1,14 +1,14 @@
 import pytest
 from unittest.mock import patch
-from AlteryxGallery import Gallery
+from AlteryxGallery import AlteryxGalleryAPI
 import json
 
 @pytest.fixture
 def gallery():
-    return Gallery(api_location='https://example.com', api_key='key', api_secret='secret')
+    return AlteryxGalleryAPI.Gallery(api_location='https://example.com', api_key='key', api_secret='secret')
 
 @patch('gallery.requests.get')
-def test_subscription(mock_get, gallery):
+def test_subscription(mock_get, gallery: AlteryxGalleryAPI.Gallery):
     mock_response = {'workflows': [{'id': 1, 'name': 'workflow1'}, {'id': 2, 'name': 'workflow2'}]}
     mock_get.return_value.status_code = 200
     mock_get.return_value.content.decode.return_value = json.dumps(mock_response)
