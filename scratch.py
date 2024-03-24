95d68ab74e9081aebd83d5a992d11f362d819fbe
Paul Houghton <paul.j.houghton@gmail.com>
2024-03-24 20:03:14 +0000

    Update API authentication scratch testing to use httpx library

--- scratch.py
+++ scratch.py
@@ -1,23 +1,31 @@
-import requests
+import httpx
 import os
 from dotenv import load_dotenv
-
+from AlteryxGallery import AlteryxGalleryAPI
 
 load_dotenv()
 
 base_url = "https://spider.theinformationlab.co.uk/webapi/"
 
-token_url = f"{base_url}oauth2/token"
+# token_url = f"{base_url}oauth2/token"
 
-url = "https://spider.theinformationlab.co.uk/webapi/v3/workflows?view=Default"
+# url = "https://spider.theinformationlab.co.uk/webapi/v3/workflows?view=Default"
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-payload = {"client_id": client_id, "client_secret": client_secret, "grant_type": "client_credentials"}
 
-response = requests.post(token_url, data=payload)
+# payload = {"client_id": client_id, "client_secret": client_secret, "grant_type": "client_credentials"}
 
-print(response.url)
-print(response.status_code)
-print(response.text)
+# response = httpx.post(token_url, data=payload)
+
+# print(response.url)
+# print(response.status_code)
+# print(response.text)
+
+
+with AlteryxGalleryAPI.GalleryClient(base_url=base_url) as client:
+    client.authenticate(client_id, client_secret)
+    response, content = client.get_all_workflows(name="00-Octopus Download Pipeline")
+    print(response.status_code)
+    print(content)
