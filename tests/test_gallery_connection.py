6e043d3ad6a88c227bbb1a56b8740874322f737f
Paul Houghton <paul.j.houghton@gmail.com>
2024-03-27 11:53:18 +0000

    Refactor test_gallery_connection.py for improved readability and maintainability

--- tests/test_gallery_connection.py
+++ tests/test_gallery_connection.py
@@ -1,8 +1,8 @@
-from http import client
 import pytest
-from AlteryxGallery import AlteryxGalleryAPI
 import os
 from dotenv import load_dotenv
+
+from AlteryxGallery import AlteryxGalleryAPI
 
 load_dotenv()
 
@@ -10,15 +10,13 @@ load_dotenv()
 @pytest.fixture(scope="module")
 def http_client():
     base_url = os.getenv("BASE_URL")
-    client_id = os.getenv("CLIENT_ID")
-    client_secret = os.getenv("CLIENT_SECRET")
     with AlteryxGalleryAPI.GalleryClient(base_url) as client:
-        client.client_id = client_id
-        client.client_secret = client_secret
+        client.client_id = os.getenv("CLIENT_ID")
+        client.client_secret = os.getenv("CLIENT_SECRET")
         yield client
 
 # Test case for the authenticate method
-def test_authenticate(http_client):
+def test_authenticate(http_client: AlteryxGalleryAPI.GalleryClient):
     # Test successful authentication
     assert http_client.authenticate(http_client.client_id, http_client.client_secret) == True
 
@@ -26,7 +24,7 @@ def test_authenticate(http_client):
     assert http_client.authenticate("incorrect_username", "incorrect_password") == False
 
 # Test case for the get_all_workflows method
-def test_get_all_workflows(http_client):
+def test_get_all_workflows(http_client: AlteryxGalleryAPI.GalleryClient):
     response, content = http_client.get_all_workflows(name="00-Octopus Download Pipeline")
     assert response.status_code == 200
     assert content[0]["name"] == "00-Octopus Download Pipeline"
@@ -34,6 +32,4 @@ def test_get_all_workflows(http_client):
     
     response, content = http_client.get_all_workflows(name="Non-existent Workflow")
     assert response.status_code == 200
-    # assert content["name"] == "Non-existent Workflow"
-    # assert len(content[0]["name"]) == 0
     assert len(content) == 0
